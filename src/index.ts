6b1ac8542ecf553ea2b8e928de2ab13cc1c3916b
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-07-18 17:24:52 +0530

    Added basic port configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World");
+import express from "express";
+import http from "http";
+import PORT from "./config/serverConfig";
+import { Server } from "socket.io";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("New user connected");
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    })
+})
+
+server.listen(PORT, () => {
+    console.log("Server is connected at the port: ", PORT);
+});
